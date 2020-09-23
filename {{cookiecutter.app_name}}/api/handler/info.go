205f5bcd4918ed259462ebc9653dbd6890fdfba1
Sergey Davidenko <sergey.davidenko@ardas.biz>
2020-09-23 21:47:25 +0300

    add model package

--- {{cookiecutter.app_name}}/api/handler/info.go
+++ {{cookiecutter.app_name}}/api/handler/info.go
@@ -3,9 +3,14 @@ package handler
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}/config"
+	"github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}/model"
 )
 
 // Info about app
 func Info(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"info": config.Conf.Core.Lable, "env": config.Conf.Core.Environment})
+	info := model.Info{
+		AppName: config.Conf.Core.Lable,
+		Env: config.Conf.Core.Environment,
+	}
+	return c.JSON(fiber.Map{info})
 }
---
+++ {{cookiecutter.app_name}}/model/model.go
@@ -0,0 +1,8 @@
+package model
+
+
+// Info struct model
+type Info struct {
+	AppName: string `json:"app_name"`
+	Env      string `json:"env"`
+}
