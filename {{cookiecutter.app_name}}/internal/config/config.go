e3d705b46e72e8c705460640049c537b790932d0
Sergey Davidenko <sergey.davidenko@ardas.biz>
2021-04-22 16:37:08 +0300

    fix cloud config

--- {{cookiecutter.app_name}}/internal/config/config.go
+++ {{cookiecutter.app_name}}/internal/config/config.go
@@ -92,9 +92,8 @@ func LoadConf(confPath string) (ConfYaml, error) {
 	viper.AutomaticEnv() // read in environment variables that match
 	viper.SetEnvPrefix("go")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	UseCloudConfig = viper.GetBool("GO_CLOUD_CONFIG")
-	if UseCloudConfig {
-		loadConfiguration(viper.GetString("GO_CLOUD_URL"), "{{cookiecutter.app_name}}", "default")
+	if viper.GetBool("GO_CLOUD_CONFIG") {
+		loadConfiguration(viper.GetString("GO_CLOUD_URL"), "example", "default")
 	}
 	if confPath != "" {
 		content, err := ioutil.ReadFile(confPath)
