7c679257e644b0b8b05a0b546783b24a6307dbd1
Sergey <31434799+SergeyDavidenko@users.noreply.github.com>
2023-11-03 16:54:00 +0200

    Update config_test.go
    fix test

--- {{cookiecutter.app_name}}/pkg/config/config_test.go
+++ {{cookiecutter.app_name}}/pkg/config/config_test.go
@@ -18,6 +18,6 @@ func TestConfig(t *testing.T) {
 	test := os.Getenv("GO_AUTH_USERNAME")
 	assert.Equal(t, "test", test)
 	assert.Equal(t, "test", cfg.Auth.Username)
-	assert.Equal(t, "host=localhost user=go password=go dbname=securitydb port=5432 sslmode=disable", cfg.BuildDSNPostgres())
+	assert.Equal(t, "host=localhost user=go password=go dbname={{cookiecutter.app_name}} port=5432 sslmode=disable", cfg.BuildDSNPostgres())
 	assert.Equal(t, "localhost:6379", cfg.GetRedis().HostString)
 }
