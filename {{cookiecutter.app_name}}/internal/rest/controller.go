0c28dc6eb6b6fb8c47bab70eab0e8597e29fe290
Sergey <31434799+SergeyDavidenko@users.noreply.github.com>
2023-11-03 17:08:39 +0200

    Update controller.go

--- {{cookiecutter.app_name}}/internal/rest/controller.go
+++ {{cookiecutter.app_name}}/internal/rest/controller.go
@@ -2,7 +2,8 @@ package rest
 
 import (
 	"{{cookiecutter.app_name}}/internal/repository"
-	"github.com/sirupsen/logrus"
+	
+	log "github.com/sirupsen/logrus"
 )
 
 type Handler struct {
@@ -10,7 +11,7 @@ type Handler struct {
 }
 
 func New(repository *repository.DB) *Handler {
-	logrus.Debug("init handler")
+	log.Debug("init handler")
 	return &Handler{
 		repo: repository,
 	}
